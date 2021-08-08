9064f84b1e8ef466155bac20cdd7e215e2ee35b5
Daniela Caus <danicaus.br@gmail.com>
2021-08-07 23:36:52 -0300

    Initial configuration of my Next-app application

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | Ig.News</title>
+      </Head>
+      <h1>Hello world</h1>
+    </>
+  )
+}
